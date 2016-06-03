a39cb59447b3b4d6b340ae0e1cd835fc01a5260c
cryanimal <kaokao1121@qq.com>
2016-06-03 17:14:45 +0800

    Text 增加点击方法

--- src/main/java/lazy/test/ui/controls/Text.java
+++ src/main/java/lazy/test/ui/controls/Text.java
@@ -35,4 +35,15 @@ public class Text extends AbstractControl implements Inputable, Clearable {
     public String getText(){
         return be.getText(getValidXpath());
     }
+    
+    /**
+	 * 进入iframe，找到控件
+	 * 点击控件
+	 * 包含重试和超时机制
+	 * 支持设置预先等待时间，以方便肉眼能跟上（通过设置pause变量）
+	 */
+	public void click() {
+        enterFrame();
+        be.click(getXpath());
+    }
 }
